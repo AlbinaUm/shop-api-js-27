f2026bb7089027aec917d6c1053cba3f1d94e8b5
Umarova Albina <albinaumarova2014@gmail.com>
2025-03-26 14:49:06 +0600

    [API - Frontend] Added image field to product

--- routers/products.ts
+++ routers/products.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import fileDb from "../fileDb";
 import {ProductWithoutId} from "../types";
+import {imagesUpload} from "../multer";
 
 const productRouter = express.Router();
 
 // products?id=5
 productRouter.get('/', async (req, res) => {
-   const products = await fileDb.getAllProducts();
+    const products = await fileDb.getAllProducts();
     const queryId = req.query.id as string;
     console.log(queryId);
     res.send(products);
@@ -17,15 +18,16 @@ productRouter.get('/:id', async (req, res) => {
     res.send(product);
 });
 
-productRouter.post('/', async (req, res) => {
-   const newProduct: ProductWithoutId = {
-     title: req.body.title,
-     description: req.body.description,
-     price: req.body.price
-   };
+productRouter.post('/', imagesUpload.single('image'), async (req, res) => {
+    const newProduct: ProductWithoutId = {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.file ? 'images/' + req.file.filename : null,
+    };
 
-   const savedNewProduct = await fileDb.addNewProduct(newProduct);
-   res.send(savedNewProduct);
+    const savedNewProduct = await fileDb.addNewProduct(newProduct);
+    res.send(savedNewProduct);
 });
 
 export default productRouter;
